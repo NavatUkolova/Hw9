d73b8a7542d19a744c7fa13527c867b9bc1a789e
Ukolova Nabat <ru.nabat@gmail.com>
2022-06-12 11:48:17 +0400

    zadacha01

---
+++ zadacha01/Program.cs
@@ -0,0 +1,9 @@
+﻿//Показать натуральные числа от M до N, N и M заданы
+void NumbersFromMtoN (int minNumber, int maxNumber)
+{
+    Console.Write(maxNumber + " " );
+    if (minNumber == maxNumber) return;
+    else NumbersFromMtoN (minNumber, maxNumber - 1);
+}
+NumbersFromMtoN (1, 50);
+Console.WriteLine();
