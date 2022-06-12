d2892cf3e2d586c6d5ff5d34e6ea2ad7855fbd98
Ukolova Nabat <ru.nabat@gmail.com>
2022-06-12 12:18:55 +0400

    zadacha02

---
+++ zadacha02/Program.cs
@@ -0,0 +1,7 @@
+﻿//Найти сумму элементов от M до N, N и M заданы
+int SumofNumbers(int M, int N)
+{
+    if(M ==N) return N;
+    else return M + SumofNumbers(M + 1, N);
+}
+Console.WriteLine(SumofNumbers(1, 10));
